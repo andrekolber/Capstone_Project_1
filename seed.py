5e33f1f559140857f5baab097e9cd1f0f7430b18
Andre Kolber <andrekolber1@gmail.com>
2021-12-21 16:05:15 -0500

    12/21/21 commit

--- seed.py
+++ seed.py
@@ -1,5 +1,15 @@
 from app import app
-from models import db
+from models import db, Stock
 
 db.drop_all()
 db.create_all()
+
+stocks = Stock.query.all()
+
+for i in range(len(stocks)):
+    stock = Stock(symbol=stocks[i]['symbol'],
+                    name=stocks[i]['name'],
+                    exchange=stocks[i]['exchange'],
+                    type=stocks[i]['type'])
+    db.session.add(stock)
+    db.session.commit()
