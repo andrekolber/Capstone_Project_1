94d0d540741dd41f5b268c51af6bba2ec6738b9f
Andre Kolber <andrekolber1@gmail.com>
2021-12-22 14:36:12 -0500

    12/22/21 commit

--- seed.py
+++ seed.py
@@ -7,10 +7,10 @@ db.create_all()
 stocks = Stock.query.all()
 
 for i in range(len(stocks)):
-    stock = Stock(symbol=stocks[i].symbol,
-                  name=stocks[i].name,
-                  exchange=stocks[i].exchange,
-                  type=stocks[i].type)
+    stock = Stock(symbol=stocks[i]['symbol'],
+                  name=stocks[i]['name'],
+                  exchange=stocks[i]['exchange'],
+                  type=stocks[i]['type'])
 
     db.session.add(stock)
     db.session.commit()
